cc54784beed01caf1b268f863cd9f8a669c91677
ArthurCarmo <arthur.goncalves.carmo@gmail.com>
2018-04-28 20:43:39 -0300

    modified:   classes/num_z/op_atrmul.cpp 	modified:   to_do

--- classes/num_z/op_atrmul.cpp
+++ classes/num_z/op_atrmul.cpp
@@ -1,12 +1,66 @@
 #include "../include/num_z.h"
 
-/*
-num_z & num_z::operator*=(const num_z &);
-num_z & num_z::operator*=(const div_tuple &a);
-num_z & num_z::operator*=(const mod_tuple &a);
-num_z & num_z::operator*=(const int64_t &);
-num_z & num_z::operator*=(const uint64_t &);
-num_z & num_z::operator*=(const int &);
-num_z & num_z::operator*=(const uint32_t &);
-num_z & num_z::operator*=(const char *a);
-*/
+
+num_z & num_z::operator*=(const num_z &a){
+	if((a == 0) | (*this == 0))return (*this = 0);
+	if(a == 1) return *this;
+	if(*this == 1) return (*this = a);
+	if(a == -1) return (*this = -*this);
+	if(*this == -1) { *this = -a; return *this;}
+	int sign = this->_sign ^ a._sign;
+	uint32_t i, j, m, n;
+	uint64_t k;
+	num_z res(uint32_t(0));
+	num_z t;
+	
+	m = a._blocks;
+	n = this->_blocks;
+	k = 0;
+	
+	res.__resize(m + n + 1);
+	
+	for(j = 0; j < m; ++j){
+		k = 0;
+		for(i = 0; i < n; ++i){
+			t = res._long_mul(this->_num[i],a._num[j]) + res._num[i+j] + k;
+			res._num[i + j] = t._mod_b();
+			k = t._div_b();
+		}
+		res._num[j+n] = k;
+	}
+	
+	res._sign = sign;
+	res._blocks = m + n;
+	while(res._num[res._blocks - 1] == 0) --res._blocks;
+	*this = res;
+	return *this;
+}
+
+num_z & num_z::operator*=(const div_tuple &a){
+	return *this *= a.q;
+}
+
+num_z & num_z::operator*=(const mod_tuple &a){
+	return *this *= a.r;
+}
+
+num_z & num_z::operator*=(const int64_t &a){
+	return *this *= (num_z)a;
+}
+
+num_z & num_z::operator*=(const uint64_t &a){
+	return *this *= (num_z)a;
+}
+
+num_z & num_z::operator*=(const int &a){
+	return *this *= (num_z)a;
+}
+
+num_z & num_z::operator*=(const uint32_t &a){
+	return *this *= (num_z)a;
+}
+
+num_z & num_z::operator*=(const char *a){
+	return *this *= (num_z)a;
+}
+
