7a7e04432df8ec2aed85a250d20e513ffaa52255
ArthurCarmo <arthur.goncalves.carmo@gmail.com>
2018-05-05 13:17:44 -0300

    renamed:    input_exemplo/inp -> input_exemplo/inp1 	new file:   input_exemplo/inp10 	renamed:    input_exemplo/inp5_inv -> input_exemplo/inp11 	renamed:    input_exemplo/out_exemplo/outg -> input_exemplo/out_exemplo/outg1 	new file:   input_exemplo/out_exemplo/outg10 	renamed:    input_exemplo/out_exemplo/outg5_inv -> input_exemplo/out_exemplo/outg11 	modified:   input_exemplo/out_exemplo/outm 	modified:   input_exemplo/teste_input.sh

--- input_exemplo/teste_input.sh
+++ input_exemplo/teste_input.sh
@@ -1,32 +1,8 @@
 #!/bin/bash
 
-echo Teste1
-../mini-cas < inp > out_exemplo/outm
-diff out_exemplo/outm out_exemplo/outg
-echo Teste2
-../mini-cas < inp2 > out_exemplo/outm
-diff out_exemplo/outm out_exemplo/outg2
-echo Teste3
-../mini-cas < inp3 > out_exemplo/outm
-diff out_exemplo/outm out_exemplo/outg3
-echo Teste4
-../mini-cas < inp4 > out_exemplo/outm
-diff out_exemplo/outm out_exemplo/outg4
-echo Teste5
-../mini-cas < inp5 > out_exemplo/outm
-diff out_exemplo/outm out_exemplo/outg5
-echo Teste5_inv
-../mini-cas < inp5_inv > out_exemplo/outm
-diff out_exemplo/outm out_exemplo/outg5_inv
-echo Teste6
-../mini-cas < inp6 > out_exemplo/outm
-diff out_exemplo/outm out_exemplo/outg6
-echo Teste7
-../mini-cas < inp7 > out_exemplo/outm
-diff out_exemplo/outm out_exemplo/outg7
-echo Teste8
-../mini-cas < inp8 > out_exemplo/outm
-diff out_exemplo/outm out_exemplo/outg8
-echo Teste9
-../mini-cas < inp9 > out_exemplo/outm
-diff out_exemplo/outm out_exemplo/outg9
+for i in {1..11};
+do
+echo Teste$i
+../mini-cas < inp$i > out_exemplo/outm
+diff out_exemplo/outm out_exemplo/outg$i
+done
