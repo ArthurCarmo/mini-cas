8fbe8d29ca95e6a7bd942b02e80bdb8b209e54b5
ArthurCarmo <arthur.goncalves.carmo@gmail.com>
2018-09-10 22:02:59 -0300

    new file:   script/polynomial_test.cpp

---
+++ script/polynomial_test.cpp
@@ -0,0 +1,22 @@
+#include <mini-cas>
+using namespace std;
+
+int main(){
+	
+	monomial mono(num_q(159, 1383), 'x', 1, 'y', 2, 'z', 3);
+	monomial unity(num_q(1), 'x', 2, 'y', 4);
+	monomial x('x');
+	
+	polynomial poly1(x, unity, mono, x, monomial('x', 2)), poly2(monomial(num_q(-2), 'x', 1), unity);
+
+	cout << poly1 << endl;
+	cout << poly2 << endl << endl;
+	
+	cout << poly1 + poly2 << endl;
+
+	poly1 += polynomial(monomial(num_q(-318, 1383), 'x', 1, 'y', 2, 'z', 3), monomial(num_q(-2), 'x', 1), monomial(num_q(-1), 'x', 2), monomial(num_q(-1), 'x', 2, 'y', 4));
+	
+	cout << poly1 << endl;
+	
+	return 0;
+}
