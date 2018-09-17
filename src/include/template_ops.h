8b88647c702c8549f241b1fd78bdb28161e40e9e
ArthurCarmo <arthur.goncalves.carmo@gmail.com>
2018-09-17 09:13:26 -0300

    modified:   Makefile 	modified:   Makefile.am 	modified:   Makefile.in 	modified:   autom4te.cache/requests 	modified:   config.log 	modified:   minicas-beta.tar.gz 	deleted:    src/include/adder.h 	modified:   src/include/mini-cas 	deleted:    src/include/multiplier.h 	new file:   src/include/template_ops.h 	new file:   stamp-h1

---
+++ src/include/template_ops.h
@@ -0,0 +1,38 @@
+#ifndef __TEMPLATE_OPS_H
+#define __TEMPLATE_OPS_H
+
+#include "num_z.h"
+
+template<class T>
+T adder(T v) {
+	return v;
+}
+
+template<class T, class... Args>
+T adder(T first, Args... args) {
+	return first + adder(args...);
+}
+
+template<class T>
+T multiplier(T v) {
+	return v;
+}
+
+template<class T, class... Args>
+T multiplier(T first, Args... args) {
+	return first * adder(args...);
+}
+
+template<class T>
+T g_pow(const T &a, const num_z &N){
+	T res;
+
+	if(N == 0) return T(1);
+	if(N == 1) return a;
+	div_tuple q = N / 2;
+	res = g_pow(a, q.q);
+	if(N._num[0] & 1) return a * res * res;
+	return res * res;
+}
+
+#endif
