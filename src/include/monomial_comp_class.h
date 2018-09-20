355bf5cd9021c2bf8f63ba59c3d337f536aba8bb
ArthurCarmo <arthur.goncalves.carmo@gmail.com>
2018-09-19 22:16:28 -0300

    modified:   .libs/libminicas.so.0.0.0 	modified:   .libs/t_polynomials 	new file:   doc/.pmini-cas05.odp-autosave.odp 	modified:   doc/pmini-cas05.odp 	modified:   doc/pmini-cas05.odp~ 	modified:   minicas-beta.tar.gz 	modified:   src/include/monomial_comp_class.h

--- src/include/monomial_comp_class.h
+++ src/include/monomial_comp_class.h
@@ -4,23 +4,73 @@
 #include "monomial.h"
 
 class monomial_comp_class{
-	public:
-		bool operator()(const monomial &l, const monomial &r) const { 
+	private:
+	
+		bool glex(const monomial &l, const monomial &r) const { 
 			std::map<std::string, num_z>::const_iterator it_l = l._literals.begin();
 			std::map<std::string, num_z>::const_iterator it_r = r._literals.begin();
 			if(l._degree > r._degree) return true;
 			if(l._degree < r._degree) return false;
-				
+		
 			do{
 				if(it_r == r._literals.end()) return false;
 				if(it_l == l._literals.end()) return true;
 				if(it_l->first < it_r->first) return true;
 				if(it_l->first > it_r->first) return false;
-				if(it_l->second > it_r->second) return true;
-				if(it_l->second < it_r->second) return false;
+				if(it_l->second < it_r->second) return true;
+				if(it_l->second > it_r->second) return false;
+		
 				++it_l;
 				++it_r;
 			}while(true);
+		}
+		
+		bool grevlex(const monomial &l, const monomial &r) const { 
+			std::map<std::string, num_z>::const_reverse_iterator it_l = l._literals.rbegin();
+			std::map<std::string, num_z>::const_reverse_iterator it_r = r._literals.rbegin();
+			if(l._degree > r._degree) return true;
+			if(l._degree < r._degree) return false;
+		
+			do{
+				if(it_l == l._literals.rend()) return false;
+				if(it_r == r._literals.rend()) return true;
+				if(it_l->first < it_r->first) return true;
+				if(it_l->first > it_r->first) return false;
+				if(it_l->second < it_r->second) return true;
+				if(it_l->second > it_r->second) return false;
+		
+				++it_l;
+				++it_r;
+			}while(true);
+		}
+
+		bool plex(const monomial &l, const monomial &r) const { 
+			std::map<std::string, num_z>::const_iterator it_l = l._literals.begin();
+			std::map<std::string, num_z>::const_iterator it_r = r._literals.begin();
+	
+			do{
+				if(it_r == r._literals.end()) return false;
+				if(it_l == l._literals.end()) return true;
+				if(it_l->first < it_r->first) return true;
+				if(it_l->first > it_r->first) return false;
+				if(it_l->second < it_r->second) return true;
+				if(it_l->second > it_r->second) return false;
+		
+				++it_l;
+				++it_r;
+			}while(true);
+		}
+
+		bool lexdeg(const monomial &l, const monomial &r) const {
+			/*
+				TO DO
+			*/
+			return true;
+		}
+
+	public:
+		bool operator()(const monomial &l, const monomial &r) const { 
+			return grevlex(l, r);
 		}
 };
 
