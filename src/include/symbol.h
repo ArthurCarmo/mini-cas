8b88ba4446c38bd318c67be02d5d49144177d55e
ArthurCarmo <arthur.goncalves.carmo@gmail.com>
2018-09-26 08:19:31 -0300

    modified:   src/include/symbol.h

--- src/include/symbol.h
+++ src/include/symbol.h
@@ -1,22 +1,8 @@
 #ifndef __SYMBOL_H_
 #define __SYMBOL_H_
 
-#include "num_q.h"
-#include <string>
-
 class symbol {
 
-	private:
-		std::string _value;
-		num_q _product;
-		num_q _sum;
-		
-	public:
-		symbol(const char *name) {
-			this->_value.assign(name);
-			this->_sum = 0;
-			this->_product = 1;
-		};
 };
 
 #endif
