99a1d3b32002e840f0802653395d780fab19292e
ArthurCarmo <arthur.goncalves.carmo@gmail.com>
2018-11-21 12:44:50 -0200

    modified:   examples/Expr_structure.cpp 	modified:   examples/exemplo

--- examples/Expr_structure.cpp
+++ examples/Expr_structure.cpp
@@ -17,7 +17,7 @@ int main(){
 		
 	Expr e5 = (x - 2) * (x - 3) / (x - 2);
 	Expr e6 = (x - 2) * ((x - 3) / (x - 2)) * (x - 3);
-	Expr e7 = (x - 2) / ((x - 3) / (x - 2)) * (x - 3);
+	Expr e7 = (x - 2) / ((x - 3) / (x * (x - 2))) * (x - 3);
 
 	cout << e5 << endl;
 	cout << e6 << endl;
